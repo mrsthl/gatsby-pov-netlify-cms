bf165b58d2c730dfb4ff53af56803430239e6d4f
mauro <maurostehle@gmail.com>
2020-02-04 23:26:22 +0100

    init

--- src/pages/about/index.md
+++ src/pages/about/index.md
@@ -1,19 +1,15 @@
 ---
 templateKey: 'about-page'
 path: /about
-title: About our values
+title: Über uns
+subTitle: Unser Team
 ---
-### Shade-grown coffee
-Coffee is a small tree or shrub that grows in the forest understory in its wild form, and traditionally was grown commercially under other trees that provided shade. The forest-like structure of shade coffee farms provides habitat for a great number of migratory and resident species.
-
-### Single origin
-Single-origin coffee is coffee grown within a single known geographic origin. Sometimes, this is a single farm or a specific collection of beans from a single country. The name of the coffee is then usually the place it was grown to whatever degree available.
-
-### Sustainable farming
-Sustainable agriculture is farming in sustainable ways based on an understanding of ecosystem services, the study of relationships between organisms and their environment. What grows where and how it is grown are a matter of choice and careful consideration for nature and communities.
-
-### Direct sourcing
-Direct trade is a form of sourcing practiced by some coffee roasters. Advocates of direct trade practices promote direct communication and price negotiation between buyer and farmer, along with systems that encourage and incentivize quality.
-
-### Reinvest profits
-We want to truly empower the communities that bring amazing coffee to you. That’s why we reinvest 20% of our profits into farms, local businesses and schools everywhere our coffee is grown. You can see the communities grow and learn more about coffee farming on our blog.
+ webTIE wurde ursprünglich von den 2 Studenten Mauro und Sven der ABB Technikerschule, später der FHNW
+              Brugg-Windisch,
+              gegründet um gemeinsam private Anfragen für Webseitenprojekte umzusetzen. <br /> Mit den Weiterempfehlungen
+              unserer Kunden konnten wir ohne Werbung und Marketing diverse Kundenaufträge erfolgreich umsetzen. Durch
+              die wachsende Anzahl der zu verwaltenden und zu betreuenden Projekte, stiegen auch die Anforderungen und
+              wir investierten in Infrastruktur und intelligente Backuplösungen.<br /> Somit sind wir heute gut gerüstet
+                und
+                können umfassende Komplettlösungen anbieten, welche von der Erstellung einer Webapplikation, über das
+                Hosting der Applikation bis zu deren Wartung führt.
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,81 +1,72 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Content, {HTMLContent} from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const AboutPageTemplate = ({title, subtitle: subTitle, content, contentComponent}) => {
+    const PageContent = contentComponent || Content
 
-  return (
-      <div className="container-fluid">
-        <section className="page-section" id="team">
-          <div className="page-title col-lg-12 text-center">
-            <h1 id="about" className="page-heading text-uppercase">Über uns</h1>
-          </div>
-          <div className="container content">
-            <div className="page-title col-lg-12 text-center">
-              <h2 className="page-heading text-uppercase">Unser Team</h2>
-            </div>
+    return (
+        <div className="container-fluid">
+            <section className="page-section" id="team">
+                <div className="page-title col-lg-12 text-center">
+                    <h1 id="about" className="page-heading text-uppercase">{title}</h1>
+                </div>
+                <div className="container content">
+                    <div className="page-title col-lg-12 text-center">
+                        <h2 className="page-heading text-uppercase">{subTitle}</h2>
+                    </div>
 
-            <div className="row text-center">
-              <div className="team-member">
-                <img src="img/mauro_webTIE.jpg" className="mx-auto rounded-circle"/>
-                <h4>Mauro Stehle</h4>
+                    <div className="row text-center">
+                        <div className="team-member">
+                            <img src="img/mauro_webTIE.jpg" className="mx-auto rounded-circle"/>
+                            <h4>Mauro Stehle</h4>
 
-                <p className="text-muted">Gründer und Entwickler</p>
-              </div>
+                            <p className="text-muted">Gründer und Entwickler</p>
+                        </div>
 
-              <div className="team-member">
-                <img src="img/sven_webTIE.jpg" className="mx-auto rounded-circle"/>
-                <h4>Sven Humbel</h4>
+                        <div className="team-member">
+                            <img src="img/sven_webTIE.jpg" className="mx-auto rounded-circle"/>
+                            <h4>Sven Humbel</h4>
 
-                <p className="text-muted">Gründer und Entwickler</p>
-              </div>
-            </div>
+                            <p className="text-muted">Gründer und Entwickler</p>
+                        </div>
+                    </div>
 
-            <div className="page-title col-lg-12 text-center">
-              <h2 className="page-heading text-uppercase">Gründung</h2>
-            </div>
+                    <div className="page-title col-lg-12 text-center">
+                        <h2 className="page-heading text-uppercase">Gründung</h2>
+                    </div>
 
-            <p className="text-muted">
-              webTIE wurde ursprünglich von den 2 Studenten Mauro und Sven der ABB Technikerschule, später der FHNW
-              Brugg-Windisch,
-              gegründet um gemeinsam private Anfragen für Webseitenprojekte umzusetzen. <br /> Mit den Weiterempfehlungen
-              unserer Kunden konnten wir ohne Werbung und Marketing diverse Kundenaufträge erfolgreich umsetzen. Durch
-              die wachsende Anzahl der zu verwaltenden und zu betreuenden Projekte, stiegen auch die Anforderungen und
-              wir investierten in Infrastruktur und intelligente Backuplösungen.<br /> Somit sind wir heute gut gerüstet
-                und
-                können umfassende Komplettlösungen anbieten, welche von der Erstellung einer Webapplikation, über das
-                Hosting der Applikation bis zu deren Wartung führt.</p>
-          </div>
-        </section>
-      </div>
-  )
+                    <p className="text-muted">{content} </p>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    contentComponent: PropTypes.func,
 }
 
-const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+const AboutPage = ({data}) => {
+    const {markdownRemark: post} = data
 
-  return (
-    <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <AboutPageTemplate
+                contentComponent={HTMLContent}
+                title={post.frontmatter.title}
+                content={post.html}
+            />
+        </Layout>
+    )
 }
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
 }
 
 export default AboutPage
@@ -86,7 +77,8 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
-      }
+        subTitle
+      }   
     }
   }
 `
